32101faa87b18e4007ae157e50a16ac2fca7062b
Benedicte des Arcis <bdesarcis@gmail.com>
2020-08-25 18:13:41 +0200

    profile-page

--- app/javascript/components/fade_text.js
+++ app/javascript/components/fade_text.js
@@ -1,9 +1,50 @@
-const fadeText = () => {
-	const terms = ["techniques", "mentales", "physiques"];
-	setInterval(() => {
-		console.log("coucou");
-	}, 2000)
-}
 
 
-export {fadeText}
+// const fadeText = () => {
+// 	// Récupérer le span
+// 	let category = document.querySelector("#textFade")
+// 	const terms = ["techniques", "mentales", "physiques"];
+// 	setInterval(() => {
+		
+// 		// Lui attribuer une classe fade-out (le faire disparaître)
+
+// 		category.classList.add("fade-out");
+// 		// Changer son texte
+		
+// 		category.innerHTML = terms[1]
+// 		category.classList.remove("fade-out");
+// 		category.classList.add("fade-in");
+		
+		
+// 		// // Lui attribuer une classe fade-in (le faire réapparaître)
+// 		// category.classList.add("fade-in");
+
+// 		// category.classList.remove("fade-in")
+// 		// setTimeout(function(){
+// 		//     //do what you need here
+// 		// setTimeout(function(){
+// 		// 	category.classList.add("fade-out");
+// 		// }, 200);
+
+// 		category.innerHTML = terms[2]
+
+// 		category.classList.remove("fade-out");
+
+// 		// category.classList.add("fade-in");
+// 		// 	category.classList.remove("fade-in")
+// 		// setTimeout(function(){
+// 		//     //do what you need here
+// 		// 	category.classList.add("fade-out");
+// 		// }, 2000);
+
+// 		// category.innerHTML = terms[2]
+// 		// // // Lui attribuer une classe fade-in (le faire réapparaître)
+// 		// category.classList.add("fade-in");
+// 		// category.classList.remove("fade-oin")
+
+
+// 	}, 2000)
+// }
+
+
+// export {fadeText}
