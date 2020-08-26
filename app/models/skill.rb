557962842859bc0c69cb598601a536eec6ff1e7c
julie collazos <juliecollazos@hotmail.com>
2020-08-26 16:36:15 +0200

    filters on index done

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,6 +3,7 @@ class Skill < ApplicationRecord
   
   belongs_to :user
   has_many :experiences, dependent: :destroy
+  has_many :sports, through: :experiences
   has_many :bookings
 
   validates :name, presence: true, inclusion: {in: Skill::NAMES}
