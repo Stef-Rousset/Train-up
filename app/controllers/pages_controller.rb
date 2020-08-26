5dd9d7d72f05d93d05e502c23d96aafc3163ef37
julie collazos <juliecollazos@hotmail.com>
2020-08-26 18:38:44 +0200

    test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,6 @@
 class PagesController < ApplicationController
 	skip_before_action :authenticate_user!, only: [ :home ]
-<<<<<<< HEAD
-<<<<<<< HEAD
 	
-=======
-
->>>>>>> 3d05955a8ee2e08c9a1b02eec8084b371aa11f80
-=======
-
->>>>>>> 3d05955a8ee2e08c9a1b02eec8084b371aa11f80
 	def home
 	end
 
