5f0bdc1662ce2f8f3149a4ec87dd1e7cb5657603
chrystèle claude giraud <giraud@therapose-formations.com>
2020-08-26 17:01:40 +0200

    reviews_controller done

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,9 +1,9 @@
 class Skill < ApplicationRecord
   NAMES = ["Entraîneur", "Préparateur mental", "Préparateur physique" ]
-  
+
   belongs_to :user
   has_many :experiences, dependent: :destroy
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :name, presence: true, inclusion: {in: Skill::NAMES}
   validates :location, presence: true
