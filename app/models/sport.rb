5f77099a8822afa9c68b07223280e1a21ee0126a
stephanie rousset <strabon@club-internet.fr>
2020-08-26 15:08:28 +0200

    finish dropdown sports

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,9 +1,5 @@
 class Sport < ApplicationRecord
 
-  SPORTS = %w[Aikido, Athlétisme, Aviron, Badminton, Boxe, Danse, Escalade, Equitation,
-    Judo, Kendo, Karaté, Kitesurf, Natation, Pilates, Plongée, Squash, Ski, Surf,
-    Tennis de table, Voile, Yoga]
-
   has_many :experiences
   has_many :skills, through: :experiences
   validates :name, presence: true
