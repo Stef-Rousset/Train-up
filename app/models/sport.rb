8afbf7d57fbf4fcb2c9016fca69b4a6960e62b4e
stephanie rousset <strabon@club-internet.fr>
2020-08-26 14:33:31 +0200

    beginning profile page

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,5 +1,12 @@
 class Sport < ApplicationRecord
+
+  SPORTS = %w[Aikido, Athlétisme, Aviron, Badminton, Boxe, Danse, Escalade, Equitation,
+    Judo, Kendo, Karaté, Kitesurf, Natation, Pilates, Plongée, Squash, Ski, Surf,
+    Tennis de table, Voile, Yoga]
+
   has_many :experiences
   has_many :skills, through: :experiences
   validates :name, presence: true
+
+
 end
