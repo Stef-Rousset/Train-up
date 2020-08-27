7d12b593b6cd31ed24f17cfd76a6fb7133db15a1
chrystèle claude giraud <giraud@therapose-formations.com>
2020-08-27 12:01:28 +0200

    modification syntaxe start & end hour

--- db/seeds.rb
+++ db/seeds.rb
@@ -84,11 +84,11 @@ bookings.each do |booking|
   booking = Booking.create!(name: booking)
 end
 
-booking_one = Booking.create!(user: user_one, skill: skill_fisrt, date: Date.parse("12/09/2020"), start_hour: 10 a.m., end_hour: 11 p.m.)
+booking_one = Booking.create!(user: user_one, skill: skill_fisrt, date: Date.parse("12/09/2020"), start_hour: DateTime.parse("12/09/2020 10:00"), end_hour: DateTime.parse("12/09/2020 10:00")
 booking_one.save!
-booking_two = Booking.create!(user: user_two, skill: skill_second, date: Date.parse("16/09/2020"), start_hour: 1 p.m., end_hour: 2 p.m.)
+booking_two = Booking.create!(user: user_two, skill: skill_second, date: Date.parse("16/09/2020"), start_hour: DateTime.parse("16/09/2020 13:00"), end_hour: DateTime.parse("12/09/2020 14:00"))
 booking_two.save!
-booking_three = Booking.create!(user: user_three, skill: skill_third, date: Date.parse("16/09/2020"), start_hour: 6 p.m., end_hour: 7 p.m.)
+booking_three = Booking.create!(user: user_three, skill: skill_third, date: Date.parse("24/09/2020"), start_hour: DateTime.parse("24/09/2020 17:00"), end_hour: DateTime.parse(("24/09/2020 18:00"))
 booking_three.save!
 
 puts "booking done"
