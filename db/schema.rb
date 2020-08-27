e96b15cb5e9d96acd8fd0233d31413856d59a657
chrystèle claude giraud <giraud@therapose-formations.com>
2020-08-27 16:04:39 +0200

    modifs faites

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2020_08_26_134224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
